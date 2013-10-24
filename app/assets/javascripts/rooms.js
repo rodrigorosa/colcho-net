167e146a6f62c017a4960099cb7171936e6524e5
Rodrigo Rosa <rodrigocrosa@gmail.com>
2013-10-24 10:09:49 -0200

    Alterado form da avaliacao para fazer chamada ajax e adicionado js de tratamento

---
+++ app/assets/javascripts/rooms.js
@@ -0,0 +1,19 @@
+$(function(){
+	var $review = $('.review');
+
+	$review.on('ajax:beforeSend', function(){
+		$(this).find('input').attr('disabled', true);
+	});
+
+	$review.on('ajax:error', function(){
+		replaceButton(this, 'icon-remove', '#B94A48');
+	});
+
+	$review.on('ajax:success', function(){
+		replaceButton(this, 'icon-ok','#468847');
+	});
+
+	function replaceButton(container, icon_class, color) {
+		$(container).find('input:submit').replaceWith($('<i/>')).addClass(icon_class).css('color', color));
+	};
+});
